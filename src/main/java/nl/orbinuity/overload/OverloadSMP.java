cb64d626c475ab87ed61db8f0baad215115d471f
Flurop <flora@ratgers.nl>
2026-06-17 10:27:12 +0200

    Load config from config folder and added function to check the settings

--- src/main/java/nl/orbinuity/overload/OverloadSMP.java
+++ src/main/java/nl/orbinuity/overload/OverloadSMP.java
@@ -3,8 +3,11 @@ package nl.orbinuity.overload;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import nl.orbinuity.overload.config.OverloadConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.LocalTime;
 
 public class OverloadSMP implements ModInitializer {
 	public static final String MODID = "overload";
@@ -14,6 +17,21 @@ public class OverloadSMP implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Welcome to the Overload SMP!");
 
+		OverloadConfig.load();
+
 		ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> !message.getString().equals("Hello from FTB Library!"));
+	}
+
+	public static boolean useNight() {
+		boolean useNight;
+
+		if (OverloadConfig.autoMode) {
+			int hour = LocalTime.now().getHour();
+			useNight = hour >= 18 || hour < 6;
+		} else {
+			useNight = OverloadConfig.nightMode;
+		}
+
+		return useNight;
 	}
 }
